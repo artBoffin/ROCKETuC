40ea1ff206f0cba29b909b9322ada9307665b036
Stefan Wendler <sw@kaltpost.de>
2012-05-24 21:56:39 +0200

    more documentation

--- libraries/processingrocket/src/rocketuc/processing/ROCKETuC.java
+++ libraries/processingrocket/src/rocketuc/processing/ROCKETuC.java
@@ -1,80 +1,88 @@
-/**
- * ##library.name##
- * ##library.sentence##
- * ##library.url##
+/* 
+ * This file is part of the JRocket firmware project
  *
- * Copyright ##copyright## ##author##
+ * Copyright (C) 2012 Stefan Wendler <sw@kaltpost.de>
  *
- * This library is free software; you can redistribute it and/or
+ * The JRocket firmware is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
+ *
+ * JRocket firmware is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA  02111-1307  USA
- * 
- * @author      ##author##
- * @modified    ##date##
- * @version     ##library.prettyVersion## (##library.version##)
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the JRocket firmware; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA.  
  */
 
 package rocketuc.processing;
 
-
 import processing.core.*;
 import rocketuc.jrocket.JRocket;
+import rocketuc.jrocket.JRocketException;
 import rocketuc.jrocket.JRocketSerial;
 import rocketuc.jrocket.comm.Packet;
 
 /**
- * This is a template class and can be used to start a new processing library or tool.
- * Make sure you rename this class as well as the name of the example package 'template' 
- * to your own library or tool naming convention.
- * 
- * @example Hello 
- * 
- * (the tag @example followed by the name of an example included in folder 'examples' will
- * automatically include the example in the javadoc.)
- *
+ * This calss implements the processing binding to ROCKETuC by
+ * extending JRocketSerial from the libjrocket library. Basically
+ * the only thing this class does is extending JRocketSerial with
+ * the things needed for processing integration.
  */
-
 public class ROCKETuC extends JRocketSerial {
 	
-	// myParent is a reference to the parent sketch
-	PApplet myParent;
+	/**
+ 	 * Reference to the parent sketch
+ 	 */
+	PApplet parent;
 	
-	public final static String VERSION = "##library.prettyVersion##";
-	
+	/**
+ 	 * Library version
+ 	 */
+	public final static String VERSION = "0.1";
 
 	/**
-	 * a Constructor, usually called in the setup() method in your sketch to
+	 * A Constructor, usually called in the setup() method in your sketch to
 	 * initialize and start the library.
 	 * 
-	 * @example Hello
-	 * @param theParent
+	 * @param parent		reference to parent skatch
+	 * @param port			serial port to throuch which to connect to MCU
+	 * @throws JRocketException
 	 */
-	public ROCKETuC(PApplet theParent, String port) throws Exception {
-		myParent = theParent;
+	public ROCKETuC(PApplet parent, String port) throws JRocketException {
+		this.parent = parent;
 		init(port);
 	}
 	
+	/**
+ 	 * Override event handler to pass events along to more
+ 	 * specific operations.
+ 	 *
+ 	 * @param	pkt		packet for event
+ 	 */
 	@Override
 	public void handleEvent(Packet pkt) {
+		// TODO: if we introduce other events then the
+		// external interrupt, we need to dispatch them here
+		// to go to different handlers ... 
 		externalInterrupt(pkt.getData()[0]);
 	}
 
+	/**
+ 	 * Handler called if external interrupt was received from MCU.
+ 	 *
+ 	 * @param pin	the pin for which the interrupt happened
+ 	 */	
 	public void externalInterrupt(short pin) {		
+		// Override in processing PDE
 	}
 	
 	/**
-	 * return the version of the library.
+	 * Return the version of the library.
 	 * 
 	 * @return String
 	 */
